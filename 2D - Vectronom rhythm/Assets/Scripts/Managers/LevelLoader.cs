ae63e54bda322cfa4c67a8403edd37da8027b0d5
PPCocainee <datt1guy408@gmail.com>
2025-07-18 16:31:06 -0700

    loading screen finished and working

---
+++ 2D - Vectronom rhythm/Assets/Scripts/Managers/LevelLoader.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void LoadLevel(int levelIndex)
+    {
+
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadAsynchronously(levelIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int levelIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+    
+}
--- 2D - Vectronom rhythm/Assets/Scripts/Menus/LevelMenu.cs
+++ 2D - Vectronom rhythm/Assets/Scripts/Menus/LevelMenu.cs
@@ -1,10 +1,16 @@
+using System.Collections;
+using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class LevelMenu : MonoBehaviour
 {
     public void OpenLevel(int levelID)
     {
         string levelName = "Level " + levelID;
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(levelName); 
     }
+
 }
+
+
