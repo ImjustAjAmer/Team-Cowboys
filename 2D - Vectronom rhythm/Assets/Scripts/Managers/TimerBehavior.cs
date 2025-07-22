5bec19ccd07315cc136df2eb39852cc271ebf03e
PPCocainee <datt1guy408@gmail.com>
2025-07-22 11:08:16 -0700

    time attack & collectables up running

--- 2D - Vectronom rhythm/Assets/Scripts/Managers/TimerBehavior.cs
+++ 2D - Vectronom rhythm/Assets/Scripts/Managers/TimerBehavior.cs
@@ -6,20 +6,28 @@ using UnityEngine.SceneManagement;
 
 public class TimerBehavior : MonoBehaviour
 {
+
+    public static TimerBehavior Instance;
+
     [SerializeField] TextMeshProUGUI timerText;
     public float remaingTime;
     private PlayerBehaviors playerBehaviors;
+    private CollectableBehaviors collectableBehaviors;
     private bool isDead = false;
     public float deathFreezeDuration = 0.5f;
     private Quaternion originalRotation;
+    public bool isTimeRunning = true;
 
     public SpriteRenderer playerSprite;
+
+    public int secondsAdded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void Awake()
     {
         playerBehaviors = GetComponent<PlayerBehaviors>();
+        collectableBehaviors = GetComponent<CollectableBehaviors>();
     }
 
     void Start()
@@ -32,22 +40,42 @@ public class TimerBehavior : MonoBehaviour
     {
         if (isDead) return;
 
-        if (remaingTime > 0)
+        if (isTimeRunning && remaingTime > 0)
         {
             remaingTime -= Time.deltaTime;
         }
         else
         {
             remaingTime = 0;
+
+            isTimeRunning = false;
+
             HandleGameOver();
             StartCoroutine(HandleGameOver());
+            UpdateTimerDisplay();
         }
 
         int minutes = Mathf.FloorToInt(remaingTime / 60);
         int seconds = Mathf.FloorToInt(remaingTime % 60);
         timerText.text = string.Format("{00:00}:{1:00}", minutes, seconds);
-  
+        
+        
     }
+
+    private void UpdateTimerDisplay()
+    {
+        if(timerText != null)
+        {
+            timerText.text = "time: " + Mathf.Ceil(remaingTime).ToString();
+        }
+    }
+
+    public void addToTimer(float timeToAdd)
+    {
+        remaingTime += timeToAdd;
+        UpdateTimerDisplay();
+    }
+
 
     public IEnumerator HandleGameOver()
     {
