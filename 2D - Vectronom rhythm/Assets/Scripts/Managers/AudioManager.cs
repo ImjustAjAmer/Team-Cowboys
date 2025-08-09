3bc68503ac136cf35bfaa36361c5dfd3b1766ce9
PPCocainee <datt1guy408@gmail.com>
2025-08-09 12:04:09 -0700

    updated some more UI in the build

--- 2D - Vectronom rhythm/Assets/Scripts/Managers/AudioManager.cs
+++ 2D - Vectronom rhythm/Assets/Scripts/Managers/AudioManager.cs
@@ -5,19 +5,18 @@ public class AudioManager : MonoBehaviour
     public static AudioManager instance;
 
     [Header("Audio Sources")]
-    public AudioSource backGroundMusic;
+
     public AudioSource SFX;
 
     [Header("Audio Clips")]
-    public AudioClip BGMs;
+
     public AudioClip Death;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        backGroundMusic.clip = BGMs;
-        backGroundMusic.Play();
+       
     }
 
     // Update is called once per frame
