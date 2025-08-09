7b4e34dfc75194a10ae69e5f57b8c651c29dc310
PPCocainee <datt1guy408@gmail.com>
2025-08-08 17:11:38 -0700

    updating

---
+++ 2D - Vectronom rhythm/Assets/GetGameWindowSize.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GetGameWindowSize : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Game Window Width: " + Screen.width);
+        Debug.Log("Game Window Height: " + Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
